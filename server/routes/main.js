64a51e2f49478d416938c7e1b6f55a8ddd91500a
Ofir Netzer <ofir.netz@gmail.com>
2023-09-20 23:07:32 +0300

    made few changes in the branch

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,11 +2,37 @@ const express = require('express');
 const path = require('path'); // Import the path module
 
 const router = express.Router();
+const app = express();
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended : true}))
+
+app.use(express.static('public'));
 
 // Routes to the right page
 router.get('', (req, res) => {
     // Use path.join to specify the correct file path
     res.sendFile(path.join(__dirname, '../../views/layouts/main.html'));
+});
+
+
+router.get('/about', (req, res) => {
+    // Use path.join to specify the correct file path
+    res.sendFile(path.join(__dirname, '../../views/index.html'));
+});
+
+app.post('/calc', function(req,res){
+    var x = parseInt(req.body.x)
+
+
+    var result = x
+
+    res.end(`<html>
+                <body>
+                    The answer is ${result}. 
+                    <a href="../../views/index.html">reset</a>
+                </body>
+            </html>`)
 });
 
 module.exports = router;
